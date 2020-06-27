3fcac3c3f5046c2d732affb930738c45ab610ac2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-06-27 21:35:58 +0200

    Move the plugin definition into the main file

--- lib/smart_proxy_rhsm.rb
+++ lib/smart_proxy_rhsm.rb
@@ -1,7 +1,14 @@
 require 'smart_proxy_rhsm/version'
-require 'smart_proxy_rhsm/plugin'
 
 module Proxy
   module Rhsm
+    # The Smart Proxy RHSM plugin. The goal of this plugin is to signal Katello
+    # that there's a RHSM API somewhere and expose it to clients. Essentially
+    # service discovery.
+    class Plugin < ::Proxy::Plugin
+      plugin :rhsm, Proxy::Rhsm::VERSION
+      validate :rhsm_base_url, url: true
+      expose_setting :rhsm_base_url
+    end
   end
 end
--- lib/smart_proxy_rhsm/plugin.rb
+++
@@ -1,9 +0,0 @@
-module Proxy
-  module Rhsm
-    class Plugin < ::Proxy::Plugin
-      plugin :rhsm, Proxy::Rhsm::VERSION
-      validate :rhsm_base_url, url: true
-      expose_setting :rhsm_base_url
-    end
-  end
-end
